6eb0a139b9f1676413e6190390ce48583608cd1d
Ashlin Darius Govindasamy <adgrules@hotmail.com>
2022-02-03 01:54:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     #metadata here
     name="JSEStocks",
-    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    version="0.0.2",
     author="Ashlin Darius Govindasamy",
     author_email="adg@adgstudios.co.ZA",
     url="https://www.adgstudios.co.za",
